c0cf45a455f38fd65d1933694a26c7bae834e3df
joao <joaoreisreis99@gmail.com>
2025-04-30 21:22:43 -0300

    uso de herança

--- sistema/sistema/views.py
+++ sistema/sistema/views.py
@@ -3,26 +3,26 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login,logout
 from django.conf import settings
 class Login(View):
-
-    def get(self,request):
-        contexto ={'mensagem': ''}
+    def get(self, request):
+        contexto = {'mensagem': '', 'base_html': 'base.html'}
         if request.user.is_authenticated:
             return redirect("/veiculo")
         else:
             return render(request, 'sistema/autenticacao.html', contexto)
-        
-    def post(self,request):
-        usuario = request.POST.get('usuario',None)
-        senha = request.POST.get('senha',None)
 
-        #verificaçao
-        user = authenticate(request,username=usuario,password=senha)
+    def post(self, request):
+        usuario = request.POST.get('usuario', None)
+        senha = request.POST.get('senha', None)
+
+        # verificação
+        user = authenticate(request, username=usuario, password=senha)
         if user is not None:
             if user.is_active:
-                login(request,user)
+                login(request, user)
                 return redirect("/veiculo")
-            return render(request,'sistema/autenticacao.html',{'mensagem': 'usuario ou senha incorretos'})
-        return render(request,'sistema/autenticacao.html', {'mensagem':'usuario incorreto'})
+            return render(request, 'sistema/autenticacao.html', {'mensagem': 'usuario ou senha incorretos', 'base_html': 'base.html'})
+        return render(request, 'sistema/autenticacao.html', {'mensagem': 'usuario incorreto', 'base_html': 'base.html'})
+    
 class Logout(View):
     def get(self,request):
         contexto = {'mensagem':''}
--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -6,3 +6,8 @@ class ListarVeiculos(ListView):
     model = Veiculo 
     context_object_name = 'veiculos'
     template_name = 'veiculo/listar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['base_html'] = 'base.html' 
+        return context
